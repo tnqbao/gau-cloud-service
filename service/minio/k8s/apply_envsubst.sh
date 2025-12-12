44bbfa5ec43abed512a56b8a125b308edf8927dc
unknown <quocbao.job106204@gmail.com>
2025-12-12 13:15:33 +0700

    rrefactor: minio proxy entry

--- service/minio/k8s/apply_envsubst.sh
+++ service/minio/k8s/apply_envsubst.sh
@@ -8,8 +8,11 @@ OUTPUT_DIR="base"
 # Create the output directory if it doesn't exist
 mkdir -p "$OUTPUT_DIR"
 
+# Define only the variables we want to substitute
+ENVSUBST_VARS='$API_DOMAIN $DASHBOARD_DOMAIN $WEB_HOSTING_BASE_DOMAIN $DEPLOY_ENV $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD'
+
 for file in "$TEMPLATE_DIR"/*; do
     filename=$(basename "$file")
-    envsubst < "$file" > "$OUTPUT_DIR/$filename"
+    envsubst "$ENVSUBST_VARS" < "$file" > "$OUTPUT_DIR/$filename"
     echo "Processed $file -> $OUTPUT_DIR/$filename"
 done
