3377651cdf3446746237666270b44b316d609d52
Tran Nguyen Quoc Bao <122146748+tnqbao@users.noreply.github.com>
2026-01-07 00:50:04 +0700

    [Feature] CORS Middleware Layer (#32)
    * refactor: restore minio service in pineline
    
    * chore: change status_url return in complete upload file
    
    * feat: add cors middleware
    
    * fix: wrong config json path
    
    * rebase: cors middleware config

--- backend/http/route/main.go
+++ backend/http/route/main.go
@@ -12,6 +12,7 @@ func SetupRouter(ctrl *controller.Controller) *gin.Engine {
 	if err != nil {
 		panic(err)
 	}
+	r.Use(middlewares.CORSMiddleware(ctrl.Config.EnvConfig))
 
 	apiRoutes := r.Group("/api/v1/cloud")
 	{
